06d56f9b7efda788bdafdba42806fd925817a965
KavyaGurram2000 <157549109+KavyaGurram2000@users.noreply.github.com>
2024-09-17 22:05:55 -0500

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,14 +1,26 @@
-from statsmodels.tsa.arima.model import ARIMA
+import statsmodels.formula.api as smf
 import pandas as pd
 
-# Load the training data
-train_data = pd.read_csv('/mnt/data/assignment_data_train.csv')
-train_data['Timestamp'] = pd.to_datetime(train_data['Timestamp'])
-train_data.set_index('Timestamp', inplace=True)
+data = pd.read_csv("https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv")
 
-# Use the ARIMA model
-model = ARIMA(train_data['trips'], order=(1, 1, 1))  # ARIMA model with p=1, d=1, q=1 as a starting example
-model_fit = model.fit()
+#explore the data
+data.head()
 
-# Display a summary of the ARIMA model
-model_fit.summary()
+#Select the data, - assign 1
+model = smf.ols("trips ~ hour", data=data)
+
+modelFit = model.fit()
+
+modelFit.summary()
+print(modelFit.summary())
+
+#use the test_data
+test_data = pd.read_csv('https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_test.csv')
+test_data = test_data[['hour']]
+test_data.head(10)
+
+#Predict with the test data if it fits.
+#(See source github open public model fitting, for not work adjust data input)
+pred = modelFit.predict(test_data)
+
+print(pred)
